3d1bdc3216b7f93fb690a848e08f71dade2eb201
umigit <umi-amanuma@die-katze.net>
2021-06-19 09:34:17 +0900

    [add]add models to schema

--- django/code/umihiko_server_v2/schema.py
+++ django/code/umihiko_server_v2/schema.py
@@ -1,6 +1,43 @@
-import graphene
+from graphene import ObjectType, List, Schema
 from graphene_django import DjangoObjectType
-from app.models import Tool
+from app.models import (
+    ProgramingLanguage,
+    Framework,
+    Database,
+    OperatingSystem,
+    Service,
+    Tool,
+)
+
+
+class ProgramingLanguageType(DjangoObjectType):
+    class Meta:
+        model = ProgramingLanguage
+        fields = ("name", "experience_period", "skilled", "like")
+
+
+class FrameworkType(DjangoObjectType):
+    class Meta:
+        model = Framework
+        fields = ("name", "experience_period", "skilled", "like")
+
+
+class DatabaseType(DjangoObjectType):
+    class Meta:
+        model = Database
+        fields = ("name", "experience_period", "skilled", "like")
+
+
+class OperatingSystemType(DjangoObjectType):
+    class Meta:
+        model = OperatingSystem
+        fields = ("name", "experience_period", "skilled", "like")
+
+
+class ServiceType(DjangoObjectType):
+    class Meta:
+        model = Service
+        fields = ("name", "experience_period", "skilled", "like")
 
 
 class ToolType(DjangoObjectType):
@@ -9,9 +46,31 @@ class ToolType(DjangoObjectType):
         fields = ("name", "experience_period", "skilled", "like")
 
 
-class Query(graphene.ObjectType):
-    all_tools = graphene.List(ToolType)
-    tool_by_name = graphene.Field(ToolType, name=graphene.String(required=True))
+class Query(ObjectType):
+    programing_languages = List(ProgramingLanguageType)
+    frameworks = List(FrameworkType)
+    databases = List(DatabaseType)
+    operating_systems = List(OperatingSystemType)
+    services = List(ServiceType)
+    tools = List(ToolType)
+
+    def resolve_programing_languages(root, info, **kwargs):
+        return ProgramingLanguage.objects.all()
+
+    def resolve_frameworks(root, info, **kwargs):
+        return Framework.objects.all()
+
+    def resolve_databases(root, info, **kwargs):
+        return Database.objects.all()
+
+    def resolve_operating_systems(root, info, **kwargs):
+        return OperatingSystem.objects.all()
+
+    def resolve_services(root, info, **kwargs):
+        return Service.objects.all()
+
+    def resolve_tools(root, info, **kwargs):
+        return Tool.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+schema = Schema(query=Query)
