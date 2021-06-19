96d6be50d5e068ba232a451fbb3ffbc7bfe48aba
umigit <umi-amanuma@die-katze.net>
2021-06-19 09:31:36 +0900

    [add]add models

--- django/code/app/models.py
+++ django/code/app/models.py
@@ -1,8 +1,138 @@
-from django.db import models
+from django.db import (
+    models,
+)
+from django.contrib.auth.models import (
+    User,
+)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    nickname = models.CharField(max_length=200, null=False, default="")
+    summary = models.TextField(
+        blank=True,
+        default="",
+    )
+    introduction = models.TextField(
+        blank=True,
+        default="",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class EnglishProfile(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    nickname = models.CharField(max_length=200, null=False, default="")
+    summary = models.TextField(
+        blank=True,
+        default="",
+    )
+    introduction = models.TextField(
+        blank=True,
+        default="",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ProgramingLanguage(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=False,
+    )
+    experience_period = models.CharField(
+        max_length=200,
+        blank=True,
+        default="",
+    )
+    skilled = models.BooleanField(default=False)
+    like = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Framework(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=False,
+    )
+    experience_period = models.CharField(
+        max_length=200,
+        blank=True,
+        default="",
+    )
+    skilled = models.BooleanField(default=False)
+    like = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Database(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=False,
+    )
+    experience_period = models.CharField(
+        max_length=200,
+        blank=True,
+        default="",
+    )
+    skilled = models.BooleanField(default=False)
+    like = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OperatingSystem(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=False,
+    )
+    experience_period = models.CharField(
+        max_length=200,
+        blank=True,
+        default="",
+    )
+    skilled = models.BooleanField(default=False)
+    like = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Service(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=False,
+    )
+    experience_period = models.CharField(
+        max_length=200,
+        blank=True,
+        default="",
+    )
+    skilled = models.BooleanField(default=False)
+    like = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Tool(models.Model):
-    name = models.CharField(max_length=200, null=False)
-    experience_period = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=200,
+        null=False,
+    )
+    experience_period = models.CharField(
+        max_length=200,
+        blank=True,
+        default="",
+    )
     skilled = models.BooleanField(default=False)
     like = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
