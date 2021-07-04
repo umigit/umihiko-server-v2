1b99173374759a718a6df4229d962a93a6bd2506
umigit <umi-amanuma@die-katze.net>
2021-07-04 15:28:53 +0900

    [update]change model and schema

--- django/code/app/admin.py
+++ django/code/app/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
-from .models import (
-    ProgramingLanguage,
-    Framework,
-    Database,
-    OperatingSystem,
-    Service,
-    Tool,
-    Profile,
-)
+from .models import Skill
 
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -22,8 +14,9 @@ class ProfileAdmin(admin.ModelAdmin):
     ordering = ("-created_at",)
 
 
-class ProgramingLanguageAdmin(admin.ModelAdmin):
+class SkillAdmin(admin.ModelAdmin):
     list_display = (
+        "category",
         "name",
         "like",
         "skilled",
@@ -34,72 +27,4 @@ class ProgramingLanguageAdmin(admin.ModelAdmin):
     ordering = ("-created_at",)
 
 
-class FrameworkAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "like",
-        "skilled",
-        "experience_period",
-        "created_at",
-        "updated_at",
-    )
-    ordering = ("-created_at",)
-
-
-class DatabaseAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "like",
-        "skilled",
-        "experience_period",
-        "created_at",
-        "updated_at",
-    )
-    ordering = ("-created_at",)
-
-
-class OperatingSystemAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "like",
-        "skilled",
-        "experience_period",
-        "created_at",
-        "updated_at",
-    )
-    ordering = ("-created_at",)
-
-
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "like",
-        "skilled",
-        "experience_period",
-        "created_at",
-        "updated_at",
-    )
-    ordering = ("-created_at",)
-
-
-class ToolAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "like",
-        "skilled",
-        "experience_period",
-        "created_at",
-        "updated_at",
-    )
-    ordering = ("-created_at",)
-
-
-# admin.site.unregister(User)
-# admin.site.register(User, UserAdmin)
-admin.site.register(ProgramingLanguage, ProgramingLanguageAdmin)
-admin.site.register(Framework, FrameworkAdmin)
-admin.site.register(Database, DatabaseAdmin)
-admin.site.register(OperatingSystem, OperatingSystemAdmin)
-admin.site.register(Service, ServiceAdmin)
-admin.site.register(Tool, ToolAdmin)
-admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Skill, SkillAdmin)
--- django/code/app/models.py
+++ django/code/app/models.py
@@ -1,118 +1,37 @@
-from django.db import (
-    models,
-)
-from django.contrib.auth.models import (
-    User,
-)
+from django.db import models
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profiles")
     locale = models.CharField(max_length=20, null=False)
     nickname = models.CharField(max_length=200, null=False, default="")
-    summary = models.TextField(
-        blank=True,
-        default="",
-    )
-    introduction = models.TextField(
-        blank=True,
-        default="",
-    )
+    summary = models.TextField(blank=True, default="")
+    introduction = models.TextField(blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.nickname
 
-class ProgramingLanguage(models.Model):
-    name = models.CharField(
-        max_length=200,
-        null=False,
-    )
-    experience_period = models.CharField(
-        max_length=200,
-        blank=True,
-        default="",
-    )
+
+class Skill(models.Model):
+    class Category(models.IntegerChoices):
+        LANGUAGE = 0, "Language"
+        FRAMEWORK = 1, "Framework"
+        DATABASE = 2, "Database"
+        OPETATING_SYSTEM = 3, "Operating System"
+        SERVICE = 4, "Service"
+        TOOL = 5, "Tool"
+
+    name = models.CharField(max_length=200, null=False)
+    category = models.IntegerField(choices=Category.choices, null=True)
+    experience_period = models.CharField(max_length=200, blank=True, default="")
     skilled = models.BooleanField(default=False)
     like = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="skills")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-class Framework(models.Model):
-    name = models.CharField(
-        max_length=200,
-        null=False,
-    )
-    experience_period = models.CharField(
-        max_length=200,
-        blank=True,
-        default="",
-    )
-    skilled = models.BooleanField(default=False)
-    like = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Database(models.Model):
-    name = models.CharField(
-        max_length=200,
-        null=False,
-    )
-    experience_period = models.CharField(
-        max_length=200,
-        blank=True,
-        default="",
-    )
-    skilled = models.BooleanField(default=False)
-    like = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class OperatingSystem(models.Model):
-    name = models.CharField(
-        max_length=200,
-        null=False,
-    )
-    experience_period = models.CharField(
-        max_length=200,
-        blank=True,
-        default="",
-    )
-    skilled = models.BooleanField(default=False)
-    like = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Service(models.Model):
-    name = models.CharField(
-        max_length=200,
-        null=False,
-    )
-    experience_period = models.CharField(
-        max_length=200,
-        blank=True,
-        default="",
-    )
-    skilled = models.BooleanField(default=False)
-    like = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Tool(models.Model):
-    name = models.CharField(
-        max_length=200,
-        null=False,
-    )
-    experience_period = models.CharField(
-        max_length=200,
-        blank=True,
-        default="",
-    )
-    skilled = models.BooleanField(default=False)
-    like = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
