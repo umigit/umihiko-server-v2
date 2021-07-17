00da02bc198b1decc5338d2959e0452f658f4139
umigit <umi-amanuma@die-katze.net>
2021-07-18 00:28:12 +0900

    [update]change config

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -2,9 +2,9 @@
 
 python manage.py migrate --noinput --settings umihiko_server_v2.settings.production
 python manage.py collectstatic --noinput --clear --settings umihiko_server_v2.settings.production
-uwsgi --ini uwsgi.ini --http 0.0.0.0:$PORT
 
 if [ "$#" -gt 0]; then
-  exec "$@"
+  $@
 fi
 
+uwsgi --ini uwsgi.ini --http 0.0.0.0:$PORT
